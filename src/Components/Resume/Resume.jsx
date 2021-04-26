74c3edb9f99717b775d1b2d56b1b9ad72833b61f
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2021-04-26 06:33:08 -0500

    feat: add all new components to resume component

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 
 // Components
+import About from './About/About';
 import Experience from './Experience/Experience';
 import Skills from './Skills/Skills';
+import Projects from './Projects/Projects';
+import Education from './Education/Education';
+import Leadership from './Leadership/Leadership';
+import Interests from './Interests/Interests';
 
 // CSS
 import './resume.scss';
 
 const Resume = () => {
   return (
+    <div className="content">
+      <About />
+
+      <Experience />
+
+      <Skills />
+
+      <Projects />
+
+      <Education />
+
+      <Leadership />
+
+      <Interests />
     </div>
   );
 };

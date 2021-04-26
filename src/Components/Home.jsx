c07c99544614594886d209f7a4ed03d9c60dc0bc
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2021-04-26 06:33:27 -0500

    feat: add new resume component to home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
 // Internal components
+import Navbar from './Navbar/Navbar';
 import Resume from './Resume/Resume';
 
 // Contexts
 import { ResumeContextProvider } from '../Contexts/ResumeContext';
 
 // CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './home.scss';
+import 'devicon/devicon.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Home = () => {
   return (
+    <ResumeContextProvider>
+      <Navbar />
+      <Resume />
+    </ResumeContextProvider>
   );
 };
 

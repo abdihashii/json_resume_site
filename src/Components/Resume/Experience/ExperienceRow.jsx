0764e2a8d51182fcc00c6bf4db402fabd95f11cd
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2022-09-04 18:48:55 -0500

    add project css

--- src/Components/Resume/Experience/ExperienceRow.jsx
+++ src/Components/Resume/Experience/ExperienceRow.jsx
@@ -20,18 +20,23 @@ const ExperienceRow = ({ experience }) => {
     <div className="resume-item d-flex flex-column flex-md-row mb-5">
       <div className="experience-content">
         <h3 className="mb-0">{position}</h3>
+
         <div className="subheading mb-0">{company}</div>
+
         <p>{area}</p>
+
         <p>
           <strong>Stack: </strong>
           {stack}
         </p>
+
         <ul>
           {highlights.map((highlight, key) => {
             return <li {...{ key }}>{highlight}</li>;
           })}
         </ul>
       </div>
+
       <div className="experience-date">
         <span className="text-primary">
           {startDate} - {endDate}
